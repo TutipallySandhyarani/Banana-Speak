a44e0abf57269efc825650c24e4b11943401c8e2
Tutipally Sandhya <sandhyatutipally@gmail.com>
2023-01-30 19:59:38 +0700

    fix: code updated

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv =  document.querySelector("#output");
+var btnTranslate = document.querySelector("#btn-translate")
+var inputText = document.querySelector("#input")
+var outputText =  document.querySelector("#output")
+var url = "https://api.funtranslations.com/translate/minion.json"
 
-function clickHandler() {
-    outputDiv.innerText = "ajsajs " + txtInput.value;
-};
+function getURL(input) {
+    return url + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something wrong with server! try again later ")
+}
+    
+function eventHandler() {
+    
+    var text=inputText.value
+    fetch(geturl(text))
+
+    .then(response => response.json())
+    .then(json => { 
+        var translatedText=json.contents.translated;
+        outputText.innerText=translatedText;
+     } )
+
+    .catch(errorHandler)
+
+}
    
-btnTranslate.addEventListener("click", clickHandler)
+btnTranslate.addEventListener("click", eventHandler)
